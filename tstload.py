032ca02093f7d18802d912c8890975164126243b
yaron haviv <yaronh@iguazio.com>
2020-09-15 01:17:10 +0300

    doc

--- tstload.py
+++ tstload.py
@@ -1,7 +1,7 @@
 import os
 import flowless
 from flowless.common import TaskRunContext, Event
-from flowless.demo_states import ModelClass, Test1Class, Test2Class
+from flowless.demo_states import *
 from flowless.loader import init_pipeline_context, pipeline_handler
 from flowless.states.router import ParallelRouter
 
@@ -10,7 +10,7 @@ os.environ['RESOURCE_NAME'] = 'f1'
 
 ctx = TaskRunContext()
 init_pipeline_context(ctx, globals(), 'p.json')
-resp = pipeline_handler(ctx, Event(body={'data': 99}))
+resp = pipeline_handler(ctx, Event(body={'data': [99]}))
 print(resp)
 
 
